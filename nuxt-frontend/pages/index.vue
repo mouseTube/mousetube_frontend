f720bbd721a780dfe2bc5bd91bbaa37a5b11a280
Nicolas Torquet <42234278+ntorquet@users.noreply.github.com>
2023-11-17 16:59:40 +0100

    Updates

--- nuxt-frontend/pages/index.vue
+++ nuxt-frontend/pages/index.vue
@@ -11,29 +11,31 @@ Code under GPL v3.0 licence
     <v-container>
       <v-row>
         <v-col>
-          <v-img :width="600" src="logo_mousetube_big.png" alt="logo mouseTube"></v-img>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
           <v-card
             variant="flat"
+            class="mx-auto"
+            max-width="800"
           >
-            <v-card-title class="text-h2">Welcome to mouseTube</v-card-title>
+            <v-img cover :width="600" src="logo_mousetube_big.png" alt="logo mouseTube"></v-img>
+            <v-card-title class="text-h2 mt-5">Welcome to mouseTube</v-card-title>
 
-            <v-card-text class="mt-2">
+            <v-card-text class="mt-4">
               This web application allows the exchange of mouse vocalisation recording files.<br />
-              <v-img :width="300" src="fair_data_principles.jpg" alt="FAIR"></v-img>
-              This new version is more  FAIR (Findable, Accessible, Interoperable, Reusable). Through mouseTube,
-              you can upload your data on a repository.<br />
+              <v-img :width="300" class="mt-3 mb-3" src="fair_data_principles.jpg" alt="FAIR"></v-img>
+              This new version is more <nuxt-link href="https://www.go-fair.org/fair-principles/" target="_blank">FAIR (Findable, Accessible, Interoperable, Reusable)</nuxt-link>.
+              Through mouseTube, you can upload your data on a repository.<br />
               You can be part of the community by clicking here.<br />
               Check out the mouseTube's publications: <br />
               <v-list>
                 <v-list-item>
-                  Torquet N., de Chaumont F., Faure P., Bourgeron T., Ey E. mouseTube – a database to collaboratively unravel mouse ultrasonic communication [version 1; peer review: 2 approved]. F1000Research 2016, 5:2332 (https://doi.org/10.12688/f1000research.9439.1)
+                  Torquet N., de Chaumont F., Faure P., Bourgeron T., Ey E. mouseTube – a database to collaboratively
+                  unravel mouse ultrasonic communication [version 1; peer review: 2 approved].
+                  F1000Research 2016, 5:2332 (<nuxt-link href="https://doi.org/10.12688/f1000research.9439.1" target="_blank">https://doi.org/10.12688/f1000research.9439.1</nuxt-link>) (2016).
                 </v-list-item>
                 <v-list-item>
-                  Ferhat A. T., Torquet N., Le Sourd A. M., de Chaumont F., Olivo-Marin J. C., Faure P., Bourgeron T., Ey E. Recording Mouse Ultrasonic Vocalizations to Evaluate Social Communication. J. Vis. Exp. (112), e53871, doi:10.3791/53871 (2016).
+                  Ferhat A. T., Torquet N., Le Sourd A. M., de Chaumont F., Olivo-Marin J. C.,
+                  Faure P., Bourgeron T., Ey E. Recording Mouse Ultrasonic Vocalizations to Evaluate Social Communication.
+                  J. Vis. Exp. (112), e53871, <nuxt-link href="https://dx.doi.org/10.3791/53871" target="_blank">doi:10.3791/53871</nuxt-link> (2016).
                 </v-list-item>
               </v-list>
 
@@ -70,7 +72,6 @@ Code under GPL v3.0 licence
 
 
 <script>
-
 import axios from "axios";
 
 export default {
