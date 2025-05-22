1622069bb4f2140f5a125b09e844ca2ec94343b1
lbouri <laurent.bouri2@gmail.com>
2025-05-22 14:19:58 +0000

    update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
   
     app: {
         head: {
-            link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
             title: 'mouseTube'
         }
     },
